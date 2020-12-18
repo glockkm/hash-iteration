a259e78769c568b782703c9f6ca92f431b7b3897
Kimberly <kmglock@mail.lipscomb.edu>
2020-12-17 19:20:46 -0600

    commit tree 239c4054109e8f67a0f9eb3a144de96a65a2ae29

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -2,10 +2,16 @@
 #   "Timmy" => 9, 
 #   "Sarah" => 6, 
 #   "Amanda" => 27
-# }
+# } # hash
 
+# write a method that operates on a hash of birthday kids and wishes them a happy birthday.
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+  # using #each to iterate over each pair of kids name/age. We are yielding the key/value pair to the block of code between the do/end keywords by assigning the variables kids_name and age in between the pipes, | |, to be the placeholders for each key/value pair.
 end
+# code the method such that it puts out to the terminal the following message for each kid:
+# "Happy Birthday #{kids_name}! You are now #{age} years old!" 
 
 
